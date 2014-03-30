5ee63e344e7b1bbe3ed625167ee1edf7bb1e3e1a
Pamela Ocampo <pamela.ocampo@gmail.com>
2014-03-29 20:13:15 -0700

    Now takes args [file] [subscriber type] [number of results]

--- hacking/common_routes.rb
+++ hacking/common_routes.rb
@@ -1,9 +1,10 @@
 require 'csv'
 def collect_routes
 	filename = ARGV.first
+	subscriber_type = ARGV[1] || "Subscriber"
 	routes = []
 	CSV.foreach(filename, :headers=>true) do |row|
-		next unless row[9] == "Subscriber"
+		next unless row[9] == subscriber_type
 		routes << {:start => row[3], :end => row[6]}
 	end
 	routes
@@ -12,7 +13,7 @@ end
 def sort_by_count
 	routes_count = collect_routes.inject(Hash.new(0)) {|h, v| h[v] += 1; h}
 	sorted_routes = routes_count.sort_by{|route| route[1]}.reverse!
-	lines = ARGV[1].to_i
+	lines = ARGV[2].to_i
 	sorted_routes = sorted_routes.take(lines)
 	sorted_routes.each do |route|
 		puts "Count: \t #{route[1]} \t Start: #{route[0][:start]} \t End: #{route[0][:end]}"
