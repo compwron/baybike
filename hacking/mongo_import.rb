1a1a7061a2bb9deb3d8e6e7ddc4eb8fe8caec9fe
Linda Goldstein <lgoldste@thoughtworks.com>
2014-03-29 23:09:24 -0700

    mongo import, now with curl

--- hacking/mongo_import.rb
+++ hacking/mongo_import.rb
@@ -3,8 +3,13 @@ dbname = 'baybike'
 # cleanup existing data
 `mongo dbname --eval "db.dropDatabase()"`
 
-# Dir.foreach("../data") do |file|
-dir = "../201402-babs-open-data"
+# download and unzip zip file
+`curl https://s3.amazonaws.com/trackerdata/201402_babs_open_data.zip`
+`unzip 201402-babs-open-data.zip`
+`mv 201402-babs-open-data data`
+
+dir = "data"
+# dir = "../201402-babs-open-data"
 Dir.foreach(dir) do |file|
   next if file == '.' or file == '..' or file == ".DS_Store"
   collection_name = file.gsub('201402_', '').gsub('_data.csv', '')
