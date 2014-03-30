fc036bc167d945ef70bc127b39767aa700966d6d
Pamela Ocampo <pamela.ocampo@gmail.com>
2014-03-29 22:13:52 -0700

    Map geolocation data to routes

--- hacking/common_routes.rb
+++ hacking/common_routes.rb
@@ -1,23 +1,52 @@
 require 'csv'
-def collect_routes
+
+class Station < Struct.new(:name, :lat, :long)
+end
+
+class Route < Struct.new(:start, :end)
+end
+
+def parse_stations
+	stations = []
+	filename = ARGV[1]
+	CSV.foreach(filename, :headers=>true, :header_converters => :symbol) do |row|
+		stations << Station.new(row[:name], row[:lat], row[:long])
+	end
+	stations
+end
+
+def map_lat_long_to_routes
 	filename = ARGV.first
-	subscriber_type = ARGV[1] || "Subscriber"
+	subscriber_type = ARGV[2] || "Subscriber"
 	routes = []
 	CSV.foreach(filename, :headers=>true) do |row|
 		next unless row[9] == subscriber_type
-		routes << {:start => row[3], :end => row[6]}
+		routes << Route.new(@stations.select{|station| station.name == row[3]}.first, @stations.select{|station| station.name == row[6]}.first)
 	end
 	routes
 end
 
+def map_count_to_routes
+	all_routes = map_lat_long_to_routes
+	routes_by_count = all_routes.inject(Hash.new(0))  { |hash, route| hash[route] += 1; hash}
+	routes_by_count
+end
+
+
 def sort_by_count
-	routes_count = collect_routes.inject(Hash.new(0)) {|h, v| h[v] += 1; h}
-	sorted_routes = routes_count.sort_by{|route| route[1]}.reverse!
-	lines = ARGV[2].to_i
+	sorted_routes = map_count_to_routes.sort_by{|route| route[1]}.reverse!
+	lines = ARGV[3].to_i
 	sorted_routes = sorted_routes.take(lines)
-	sorted_routes.each do |route|
-		puts "Count: \t #{route[1]} \t Start: #{route[0][:start]} \t End: #{route[0][:end]}"
+	write_to_csv(sorted_routes) if ARGV[4].to_s == "save"
+	sorted_routes
+end
+
+def write_to_csv(lines)
+	CSV.open("results.csv", "w") do |csv|
+		csv << ["count","start","start_lat","start_long","end","end_lat","end_long"]
+		lines.each {|line| csv << [line[1], line[0][:start][:name], line[0][:start][:lat], line[0][:start][:long], line[0][:end][:name], line[0][:end][:lat], line[0][:end][:long]]}
 	end
 end
 
+@stations = parse_stations
 sort_by_count
