95de25edb51bc8db7c021582afe8df37b554b7f5
Pamela Ocampo <pamela.ocampo@gmail.com>
2014-04-03 20:38:12 -0700

    Pam/Pete - bike romance

---
+++ hacking/couples.rb
@@ -0,0 +1,71 @@
+require 'pry'
+require 'date'
+
+trips = '../datasets/201402_trip_data.csv'
+
+class Trip
+    attr_reader :start_time,:end_time,:start_station,:end_station, :bike, :zip
+    def initialize(raw_data)
+        @start_time = Trip.parse_silly_date_time(raw_data[2])
+        @start_station = raw_data[4]
+        @end_time = Trip.parse_silly_date_time(raw_data[5])
+        @end_station = raw_data[7]
+        @bike = raw_data[8]
+        @zip = raw_data[10]
+    end
+
+    def self.parse_silly_date_time(str)
+        DateTime.strptime(str, '%m/%d/%Y %H:%M')
+    end
+
+    def start_end_pair
+        "#{start_station} #{end_station}"
+    end
+
+    def is_coupled_to(other)
+        difference = abs_time_delta( start_time, other.start_time ) + abs_time_delta( end_time, other.end_time )
+        difference < (10*60) # 10 mins
+    end
+
+    def to_s
+        "[#{start_station},#{end_station}] (#{bike}) #{start_time} => #{end_time}     #{zip}"
+    end
+
+    private
+    def abs_time_delta(a,b)
+        (a.to_time.to_i - b.to_time.to_i).abs
+    end
+end
+
+all_the_trips = File.open(trips).drop(1).map{ |line|
+    Trip.new(line.split(","))
+}
+
+start_end_buckets = {}
+all_the_trips.each do |trip|
+    bucket_id = trip.start_end_pair
+    start_end_buckets[bucket_id] ||= []
+    start_end_buckets[bucket_id] << trip
+end
+
+romance = []
+start_end_buckets.each do |bucket_id , trips|
+    likely_couples = []
+    trips.sort_by{ |trip| trip.start_time }.each_cons(2) do |pair|
+        first,second = pair
+        # find all trips which differ by less than 10 mins
+        if first.is_coupled_to(second)
+            likely_couples << pair
+        end
+    end
+
+    romance += likely_couples
+end
+
+romance.each do |trip_pair|
+    puts trip_pair
+    puts
+end
+
+
+binding.pry
